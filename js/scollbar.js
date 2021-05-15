ae95d6e445a08ae92d7e1e285fbecb2e77604504
vangona <rlarhksrud12@naver.com>
2021-05-15 22:29:12 +0900

    2021.05.15 update
    - url body scrollbar update
    - main body max-height update

---
+++ js/scollbar.js
@@ -0,0 +1,12 @@
+const MAINBODY = document.querySelector(".main-body");
+const HOVER = "hover";
+
+function handleMouseover() {
+    MAINBODY
+}
+
+function init() {
+    MAINBODY.addEventListener("mouseover", handleMouseover);
+}
+
+init();
