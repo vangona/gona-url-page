ff5ad4d551ebe226dc782d5e967e7bc71af1be1b
vangona <rlarhksrud12@naver.com>
2021-07-22 18:36:33 +0900

    2021.07.22 update

--- js/nav.js
+++ js/nav.js
@@ -6,30 +6,51 @@ const navCoding = document.querySelector(".main-coding");
 const navArt = document.querySelector(".main-art");
 const navStartup = document.querySelector(".main-startup");
 
+const nav = document.querySelector(".nav")
+
 const SHOWING_CN = "showing"
 
 function handleCodingClick() {
     navArt.classList.remove(SHOWING_CN);
     navStartup.classList.remove(SHOWING_CN);
     navCoding.classList.add(SHOWING_CN);
+    nav.classList.remove(SHOWING_CN)
 }
 
 function handleArtClick() {
     navStartup.classList.remove(SHOWING_CN);
     navCoding.classList.remove(SHOWING_CN);
     navArt.classList.add(SHOWING_CN);
+    nav.classList.remove(SHOWING_CN)
 }
 
 function handleStartupClick() {
     navArt.classList.remove(SHOWING_CN);
     navCoding.classList.remove(SHOWING_CN);
     navStartup.classList.add(SHOWING_CN);
+    nav.classList.remove(SHOWING_CN)
+}
+
+function close() {
+    navCoding.addEventListener("click", () => {
+        navCoding.classList.remove(SHOWING_CN)
+        nav.classList.add(SHOWING_CN)
+    })
+    navArt.addEventListener("click", () => {
+        navArt.classList.remove(SHOWING_CN)
+        nav.classList.add(SHOWING_CN)
+    })
+    navStartup.addEventListener("click", () => {
+        navStartup.classList.remove(SHOWING_CN)
+        nav.classList.add(SHOWING_CN)
+    })
 }
 
 function init() {
     navCodingLink.addEventListener("click", handleCodingClick);
     navArtLink.addEventListener("click", handleArtClick);
     navStartupLink.addEventListener("click", handleStartupClick);
+    close();
 }
 
 init();
